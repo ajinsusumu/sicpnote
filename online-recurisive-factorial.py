94e40de3d715413911728eaa0760b21157279f45
yj <you@example.com>
2018-06-15 11:37:18 +0800

    python equivalent

---
+++ online-recurisive-factorial.py
@@ -0,0 +1,24 @@
+# *Exercise 4.21:* Amazingly, Louis's intuition in *Note Exercise
+# 4-20:: is correct.  It is indeed possible to specify recursive
+# procedures without using `letrec' (or even `define'), although the
+# method for accomplishing this is much more subtle than Louis
+# imagined.  The following expression computes 10 factorial by
+# applying a recursive factorial procedure:(4)
+
+#      ((lambda (n)
+#         ((lambda (fact)
+#            (fact fact n))
+#          (lambda (ft k)
+#            (if (= k 1)
+#                1
+#                (* k (ft ft (- k 1)))))))
+#       10)
+
+
+print(
+    (lambda n: (
+        (lambda fact: fact(fact,n))
+        (lambda ft,k: 1 if k==1 else k*ft(ft,k-1))
+    ))(10)
+    )
+
